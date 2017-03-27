8fb0d651240db2a73b9c9828cc51556257db1f85
michaelzxu <xu.michael91@gmail.com>
2017-03-26 22:01:54 -0400

    deepforest 2 update

--- R/deepforest.R
+++ R/deepforest.R
@@ -1,11 +1,14 @@
 deepforest <- function(x,
                        y,
+                       x_test = NULL,
+                       y_test = NULL,
                        loss = "MSE",
-                       metric = "error",
+                       metric = Metrics::mse,
                        nlayer = 5,
                        #Forest
                        nforest = 4,
                        ntree = 100,
+                       rnd = 10,
                        nclass = 2,
                        #Tree
                        tree = "random",
@@ -17,13 +20,23 @@ deepforest <- function(x,
                        optimizer = "GradientDescent",
                        optparam = list(learning_rate = 0.1)) {
     .D <- as.matrix(x)
+    if (is.null(x_test)){
+        .T <- .D
+        y_test <- y
+    } else {
+        .T <- as.matrix(x_test)
+    }
     if (is.null(colnames(.D))) {
         colnames(.D) <- paste0("X", 1:dim(.D)[2])
+    }
+    if (is.null(colnames(.T))) {
+        colnames(.T) <- paste0("X", 1:dim(.T)[2])
     }
     .W <- array(init, dim = c(nlayer, nforest, ntree * nclass))
     .M <- vector("list", nlayer)
     for (.l in 1:nlayer) {
         .D.l <- matrix(0, nrow = dim(x)[1], ncol = as.integer(nforest * nclass))
+        .T.l <- matrix(0, nrow = dim(x_test)[1], ncol = as.integer(nforest * nclass))
         .Forest <- rep(list(vector("list", ntree)), nforest)
         for (.f in 1:nforest) {
             cat(paste0("Layer: ", .l, ", Forest: ", .f, ", Progress: |-"))
@@ -35,7 +48,7 @@ deepforest <- function(x,
                 .prog <- .prog + 1
                 .Forest[[.f]][[.t]] <- .grow_tree(.D[.tree[[1]][ ,.t],
                                                      .tree[[2]][ ,.t]],
-                                                  as.factor(y[.tree[[1]][ ,.t]]))
+                                                  y[.tree[[1]][ ,.t]],rnd)
                 if (.prog %% ceiling(ntree/10) == 0) cat("-")
             }
             cat("| done!\n")
@@ -47,20 +60,25 @@ deepforest <- function(x,
             #     .W[.l, , ] <- .optimize(.D[.i, ], y[.i], .W[.l, , ], .Forest)
             # }
             .pred <- .predict(.Forest[[.f]], .D)
+            .pred.t <- .predict(.Forest[[.f]], .T)
             # .W[.l, .f, ] <- as.numeric(optimx::optimx(.W[.l,.f, ],method = "BFGS",
             #                                fn = function(x) Metrics::mse(y,.predict(.Forest[[.f]], .D, x)))[,1:ntree])
-            .W[.l, .f, ] <- sgd::sgd(.pred,y=y,model="lm")[["coefficients"]]
+            # .W[.l, .f, ] <- sgd::sgd(.pred,y=y,model="lm")[["coefficients"]]
+            .W[.l, .f, ] <- rep(1, ntree * nclass)
             cat("| done!\n")
             #.pred <- .predict(.Forest[[.f]], .D)
             #cat(paste0("  Loss: ", .printtab(.optloss(.pred, y, loss))))
             #cat(paste0("  Eval: ", .printtab(.metric(.pred, y, metric))))
-            cat(paste0("  Eval: ",Metrics::mse(y, .pred %*% .W[.l, .f, ])))
+            cat(paste0("  Eval: ",metric(y_test, .pred.t %*% .W[.l, .f, ])))
             .D.l[ ,((.f - 1) * nclass + 1):(.f * nclass)] <- .pred %*% .W[.l,.f, ]
+            .T.l[ ,((.f - 1) * nclass + 1):(.f * nclass)] <- .pred.t %*% .W[.l,.f, ]
             .M[[.l]] <- .Forest
             cat("\n")
         }
         .D <- .D.l
+        .T <- .T.l
         colnames(.D) <- paste0("X", 1:dim(.D)[2])
+        colnames(.T) <- paste0("X", 1:dim(.T)[2])
     }
     return(structure(list(forests = .M, weights = .W), class = "deepforest"))
 }
@@ -76,18 +94,23 @@ deepforest <- function(x,
     return(out)
 }
 
-.grow_tree <- function(x, y) {
-    return(C50::C5.0(x = x, y = y))
+.grow_tree <- function(x, y, rnd) {
+    x_dm <- xgboost::xgb.DMatrix(data=x, label=y)
+    return(xgboost::xgb.train(data=x_dm, nrounds=1, verbose=0, objective="binary:logistic",
+                              params=list(booster="gbtree",eta=0.1,max_depth=99999,
+                                          max_leaves=99999,colsample_bytree=1,colsample_bylevel=0.5,
+                                          num_parallel_tree=rnd)))
+    # return(C50::C5.0(x = x, y = y))
 }
 
 .predict <- function(object, newdata, weights = rep(1, length(object))) {
     # lev <- length(object[[1]][["levels"]])
     out <- matrix(nrow = nrow(newdata),
                   ncol = length(object))
-    cat("  ")
+    # cat("  ")
     for (i in 1:length(object)) {
-        cat(paste0(i,".."))
-        out[ ,i] <- predict(object[[i]], newdata,type="prob")[,2]
+        # cat(paste0(i,".."))
+        out[ ,i] <- predict(object[[i]], newdata)
     }
     if (all(unique(weights) != 1)) {
         out <- (out %*% weights) / sum(weights)
@@ -100,7 +123,7 @@ deepforest <- function(x,
 }
 
 predict.deepforest <- function(dfobject, newdata) {
-    .D <- newdata
+    .D <- as.matrix(newdata)
     for (.l in 1:length(dfobject$forests)) {
         .D.l <- matrix(nrow = nrow(newdata), ncol = length(dfobject$forests[[.l]]))
         for (.f in 1:length(dfobject$forests[[.l]])) {
@@ -111,15 +134,36 @@ predict.deepforest <- function(dfobject, newdata) {
         .D <- .D.l
         colnames(.D) <- paste0("X", 1:dim(.D)[2])
     }
-    return(rowSums(.D)/ncol(.D))
+    return(rowSums(.D) / ncol(.D))
 }
 
-mod<-deepforest(x=as.matrix(agaricus.train$data),y=agaricus.train$label,nclass=1,nforest=5,ntree=100)
-predict(mod, as.matrix(agaricus.test$data))
+mod<-deepforest(x=as.matrix(agaricus.train$data),y=agaricus.train$label,x_test = as.matrix(agaricus.test$data), y_test = agaricus.test$label,
+                nclass=1,nforest=5,ntree=10,rnd=10)
 dfout<-predict(mod, as.matrix(agaricus.test$data))
+Metrics::auc(agaricus.test$label,dfout)
+Metrics::logLoss(agaricus.test$label,dfout)
 
-iris_train<-sample(150)[1:100]
-mod2<-deepforest(x=as.matrix(iris[iris_train,1:4]),y=as.integer(iris[iris_train,5]=="setosa"),nclass=1,nforest=5,ntree=10,nlayer=3)
-dfout2<-predict(mod2, iris[-iris_train,1:4])
-round(dfout2)
-as.integer(iris[-iris_train,5]=="setosa")
+xgb<-xgboost::xgboost(data=agaricus.train$data,label=agaricus.train$label,nround=100)
+dfout2<-predict(xgb, agaricus.test$data)
+Metrics::auc(agaricus.test$label,dfout2)
+Metrics::logLoss(agaricus.test$label,dfout2)
+
+
+credc <- read.csv("../../creditcard.csv")
+intrain <- sample(nrow(credc))[1:as.integer(nrow(credc)*0.1)]
+credc.train <- credc[intrain,which(!names(credc)=="Class")]
+credc.test <- credc[-intrain,which(!names(credc)=="Class")]
+
+credcmod<-deepforest(x=as.matrix(credc.train),y=credc[intrain,"Class"],
+                     x_test=as.matrix(credc.test),y_test=credc[-intrain,"Class"],
+                     nlayer=5,nclass=1,nforest=5,ntree=100,rnd=1,metric=Metrics::auc)
+hout<-predict(credcmod, as.matrix(credc.test))
+Metrics::auc(credc[-intrain,"Class"],hout)
+Metrics::mse(credc[-intrain,"Class"],hout)
+caret::confusionMatrix(credc[-intrain,"Class"],as.integer(round(hout)))
+
+xgb<-xgboost::xgboost(data=as.matrix(credc.train),label=credc[intrain,"Class"],nround=20)
+hout2<-predict(xgb,as.matrix(credc.test))
+Metrics::auc(credc[-intrain,"Class"],hout2)
+Metrics::mse(credc[-intrain,"Class"],hout2)
+caret::confusionMatrix(credc[-intrain,"Class"],as.integer(round(hout2)))
--- R/deepforest2.R
+++ R/deepforest2.R
@@ -1,6 +1,8 @@
 deepforest <- function(x, y, x_val = NULL, y_val =NULL, nlayer = 5, nmeta = 4,
                        nclass = NULL, nfold = 5, index = NULL,
                        eval_metric = NULL, missing = NA, metaparam = NULL,
+                       ntree = 10, subsample = 0.632, colsample = 0.2,
+                       printby = "fold", colsample_bylayer = 0.5,
                        ...) {
     if (is.null(index)) {
         index <- caret::createFolds(y, k = nfold, list = TRUE)
@@ -50,50 +52,83 @@ deepforest <- function(x, y, x_val = NULL, y_val =NULL, nlayer = 5, nmeta = 4,
         }
     }
 
+    orig_x <- x
+    orig_x_val <- x_val
+
+    .lcols <- sample(ncol(x))[1:floor(ncol(x) * colsample_bylayer)]
+    x <- x[ , .lcols]
+    x_val <- x_val[ , .lcols]
+
+    .strong <- vector("list", nlayer)
+    .strongeval <- numeric(nlayer)
     .model <- rep(list(rep(list(vector("list", nfold)), nmeta)), nlayer)
     .eval <- array(dim = c(nlayer, nmeta, nfold))
 
     for (.l in 1:nlayer) {
-        .colnm <- paste0("L", .l, "_M", rep(1:nmeta, each = nclass),
-                         "_C", rep(1:nclass, nmeta))
-        .x.l <- matrix(nrow = nrow(x), ncol = nmeta * nclass,
+        .colnm <- paste0("L", .l, "_M", rep(1:nmeta, each =
+                                                ifelse(nclass == 2, 1, nclass)),
+                         "_C", rep(1:ifelse(nclass == 2, 1, nclass), nmeta))
+        .x.l <- matrix(nrow = nrow(x), ncol = nmeta * ifelse(nclass == 2, 1,
+                                                             nclass),
                        dimnames = list(NULL, .colnm))
-        .x_val.l <- matrix(nrow = nrow(x_val), ncol = nmeta * nclass,
+        .x_val.l <- matrix(nrow = nrow(x_val), ncol = nmeta *
+                               ifelse(nclass == 2, 1, nclass),
                            dimnames = list(NULL, .colnm))
+        .csbt <- ifelse(colsample * ncol(x) <= 1, 1 / ncol(x), colsample)
+        .mxd <- 99999
+        .mxl <- 99999
+        .subs <- subsample
+        .ntree <- ntree
+
         for (.m in 1:nmeta) {
             x_val.m <- xgboost::xgb.DMatrix(data = x_val,label = y_val,
                                             missing = missing)
+            if (printby == "meta") {
+                cat(paste0("Layer ", .l, ", Meta ", .m))
+            }
             for (.f in 1:nfold) {
-                cat(paste0("Layer ", .l, ", Meta ", .m, ", Fold ", .f))
+                if (printby == "fold") {
+                    cat(paste0("Layer ", .l, ", Meta ", .m, ", Fold ", .f))
+                }
                 x.f <- xgboost::xgb.DMatrix(data = x[-index[[.f]], ],
                                             label = y[-index[[.f]]],
                                             missing = missing)
                 if (nclass == 1) {
                     .model[[.l]][[.m]][[.f]] <- xgboost::xgb.train(
-                        data = x.f, nrounds = 1, colsample_bytree=0.1,
-                        num_parallel_tree = 1000, max_depth=5,
+                        data = x.f, nrounds = 1,
+                        params = list(colsample_bytree = .csbt,
+                        subsample = .subs, eta = 1, max_leaves = .mxl,
+                        num_parallel_tree = .ntree, max_depth = .mxd),
                         watchlist = list(val = x_val.m),
-                        objective = "reg:linear", verbose = FALSE, ... = ...)
+                        objective = "reg:linear", verbose = FALSE, ...)
+                    .x.l[index[[.f]], .m] <- predict(.model[[.l]][[.m]][[.f]],
+                                                     x[index[[.f]], ])
                 } else if (nclass == 2) {
                     .model[[.l]][[.m]][[.f]] <- xgboost::xgb.train(
-                        data = x.f, nrounds = 1, colsample_bytree=0.1,
-                        num_parallel_tree = 1000, max_depth=5,
+                        data = x.f, nrounds = 1,
+                        params = list(colsample_bytree = .csbt,
+                        subsample = .subs, eta = 1, max_leaves = .mxl,
+                        num_parallel_tree = .ntree, max_depth = .mxd),
                         watchlist = list(val = x_val.m),
                         objective = "binary:logistic", verbose = FALSE,
-                        ... = ...)
+                        ...)
+                    .x.l[index[[.f]], .m] <- predict(.model[[.l]][[.m]][[.f]],
+                                                     x[index[[.f]], ])
                 } else if (nclass > 2) {
                     .model[[.l]][[.m]][[.f]] <- xgboost::xgb.train(
-                        data = x.f, nrounds = 1, colsample_bytree=0.1,
-                        num_parallel_tree = 1000, max_depth=5,
+                        data = x.f, nrounds = 1,
+                        params = list(colsample_bytree = .csbt,
+                        subsample = .subs, eta = 1, max_leaves = .mxl,
+                        num_parallel_tree = .ntree, max_depth = .mxd),
                         num_class = nclass, objective = "multi:softprob",
                         watchlist = list(val = x_val.m),
-                        verbose = FALSE, ... = ...)
+                        verbose = FALSE, ...)
+                    .x.l[index[[.f]], ((.m - 1) * nclass + 1):(.m * nclass)] <-
+                        predict(.model[[.l]][[.m]][[.f]], x[index[[.f]], ],
+                                reshape = TRUE)
                 } else {
                     stop("Invalid nclass")
                 }
-                .x.l[index[[.f]], ((.m - 1)*nclass + 1):(.m * nclass)] <-
-                    predict(.model[[.l]][[.m]][[.f]], x[index[[.f]], ],
-                            reshape = TRUE)
                 if (nclass > 2) {
                     .eval[.l, .m, .f] <- eval_metric(y_val,apply(
                         predict(.model[[.l]][[.m]][[.f]], x_val.m, reshape = TRUE),
@@ -103,23 +138,88 @@ deepforest <- function(x, y, x_val = NULL, y_val =NULL, nlayer = 5, nmeta = 4,
                         eval_metric(y_val, predict(.model[[.l]][[.m]][[.f]],
                                                    x_val.m))
                 }
-
-                cat(paste0(", Eval ", .eval[.l, .m, .f], "\n"))
+                if (printby == "fold") {
+                    cat(paste0(", Eval ", .eval[.l, .m, .f], "\n"))
+                }
+                gc()
             }
-            .x_val.l[ , ((.m - 1) * nclass + 1):(.m * nclass)] <-
-                apply(simplify2array(lapply(.model[[.l]][[.m]], predict,
-                                            newdata = x_val.m, reshape = TRUE)),
-                      1:nmeta, mean)
+            if (printby %in% c("fold","meta")) {
+                cat(paste0(", Eval ", mean(.eval[.l, .m, ]), "\n"))
+            }
+            if (nclass <= 2) {
+                .x_val.l[ , .m] <-
+                    apply(simplify2array(lapply(.model[[.l]][[.m]], predict,
+                                                newdata = x_val.m)),1,mean)
+            } else if (nclass > 2) {
+                .x_val.l[ , ((.m - 1) * nclass + 1):(.m * nclass)] <-
+                    apply(simplify2array(
+                        lapply(.model[[.l]][[.m]], predict, newdata = x_val.m)),
+                        1:nmeta, mean)
+            }
+            gc()
         }
-        x <- .x.l
-        x_val <- .x_val.l
+
+        if (.l == 1) {
+            .strong[[.l]] <-
+                xgboost::xgb.train(data = xgboost::xgb.DMatrix(data = .x.l,
+                                                               label = y,
+                                                               missing = missing),
+                                   nrounds = 20, objective = "binary:logistic",
+                                   params = list(eta = 0.3, subsample = 0.5,
+                                                 colsample_bytree = max(1/ncol(x.tmp),0.5),
+                                                 max_depth = 5),
+                                   verbose = FALSE)
+            .strongeval[.l] <- eval_metric(y, predict(.strong[[.l]], .x.l))
+            tmpscore <- eval_metric(y_val, predict(.strong[[.l]], .x_val.l))
+        } else {
+            .strong[[.l]] <-
+                xgboost::xgb.train(data = xgboost::xgb.DMatrix(data = cbind(x.tmp, .x.l),
+                                                               label = y,
+                                                               missing = missing),
+                                   nrounds = 20, objective = "binary:logistic",
+                                   params = list(eta = 0.3, subsample = 0.5,
+                                                 colsample_bytree = max(1/ncol(x),0.5),
+                                                 max_depth = 5),
+                                   verbose = FALSE)
+            .strongeval[.l] <- eval_metric(y, predict(.strong[[.l]], cbind(x.tmp, .x.l)))
+            tmpscore <- eval_metric(y_val, predict(.strong[[.l]], cbind(x_val.tmp, .x_val.l)))
+        }
+
+        .lcols <- sample(ncol(orig_x))[1:floor(ncol(orig_x) * colsample_bylayer)]
+        if (.l == 1) {
+            x.tmp <- .x.l
+            x_val.tmp <- .x_val.l
+        } else {
+            if (.strongeval[.l] > .strongeval[.l - 1]) {
+                x.tmp <- cbind(x.tmp, .x.l)
+                x_val.tmp <- cbind(x_val.tmp, .x_val.l)
+            }
+        }
+        x <- cbind(x.tmp, orig_x[ , .lcols])
+        x_val <- cbind(x_val.tmp, orig_x_val[ , .lcols])
+
+        # .lcols <- sample(ncol(orig_x))[1:floor(ncol(orig_x) * colsample_bylayer)]
+        # x <- cbind(.x.l, orig_x[ , .lcols])
+        # x_val <- cbind(.x_val.l, orig_x_val[ , .lcols])
+
+        cat(paste0("Layer ", .l, " average: ", mean(.eval[.l, , ]), "\n"))
+
+        cat(paste0("Layer ", .l, " expert: ", tmpscore, "\n"))
+        gc()
     }
-    return(structure(list(model = .model, eval = .eval)))
+    return(structure(list(model = .model, eval = .eval), class = "deepforest"))
 }
 
-mod<-deepforest(credc.train,credc[intrain,"Class"],credc.test,credc[-intrain,"Class"])
+mod <- deepforest(credc.train,credc[intrain,"Class"],
+                  credc.test,credc[-intrain,"Class"],
+                  nmeta=4, nlayer=10, nfold = 4,
+                  ntree = 1000, colsample = 0.6, subsample = 0.1, printby = "meta",
+                  colsample_bylayer = 0.2)
 
 
+a<-xgboost::xgb.DMatrix(data=as.matrix(iris[,1:4],label=as.numeric(iris[,5]=="setosa")))
+mod<-xgboost::xgb.train(data= a, nrounds=1,num_parallel_tree = 3,colsample_bytree=0.25,objective="binary:logistic")
+a<-xgboost::xgb.DMatrix(data=as.matrix(iris[,1:4],label=as.integer(iris[,5])))
 mod<-xgboost::xgb.train(data= a, nrounds=1,num_parallel_tree = 3,colsample_bytree=0.25,objective="multi:softprob",num_class=3)
 predict(mod, a, predleaf=T)
 predict(mod, a, reshape=TRUE)
